9402515f02f187a73d0e908d0753a1ab7bd3d669
bsbbss <boshenghuang01@gmail.com>
2025-10-07 01:52:36 +0800

    add .s file

---
+++ q1-uf8.s
@@ -0,0 +1,43 @@
+.data
+.text
+# .globl main
+main:
+    li a0, 0x00200000   # 測一組
+    jal ra, clz
+    # 結果在 a0，便於觀察/斷點
+halt:
+    j halt
+# uint32_t clz(uint32_t x)
+# a0: x (input/output)
+# return a0 = count of leading zeros
+clz:
+    li   t0, 32              # n = 32
+    li   t1, 16              # c = 16
+
+clz_loop:
+    srl  t2, a0, t1          # y = x >> c
+    beq  t2, x0, skip_update # if (y == 0) skip update
+    sub  t0, t0, t1          # n = n - c
+    mv   a0, t2              # x = y
+
+skip_update:
+    srli t1, t1, 1           # c = c / 2
+    bnez t1, clz_loop        # while (c) loop
+    sub  a0, t0, a0          # return n - x
+    ret
+
+uf8_decode:
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
