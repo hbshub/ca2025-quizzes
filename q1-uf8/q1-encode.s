4645db8d5cc86d5fa5fcaf5c9f44acc618d3734b
bsbbss <boshenghuang01@gmail.com>
2025-10-08 10:00:40 +0800

    fix ra calling convention

--- q1-uf8/q1-encode.s
+++ q1-uf8/q1-encode.s
@@ -1,15 +1,17 @@
 .data
 test_in:
     # .byte 0x00, 0x10, 0x1F, 0x20, 0x2F, 0x7F, 0xF0, 0xFF
-    .word 0, 1, 15, 16, 17, 31, 32, 111
+    # .word 0, 1, 15, 16, 17, 31, 32, 111
+    .word 17, 31, 32, 111
 test_out:
-    .byte 0,0,0,0,0,0,0,0
+    # .byte 0,0,0,0,0,0,0,0
+    .byte 0,0,0,0
 
 .text
 main:
     la   s0, test_in
     la   s1, test_out
-    li   s2, 8
+    li   s2, 4
 
 loop:
     beqz s2, halt
@@ -31,15 +33,17 @@ uf8_encode:
     li t3, 16
     blt a0, t3, e_0         # if (a0 < 16)
 
-    addi sp, sp, -4
-    sw   a0, 0(sp)          # store a0 in stack
+    addi sp, sp, -8
+    sw a0, 0(sp)          # store a0 in stack
+    sw ra, 4(sp)          # store ra in stack  
 
     jal  ra, clz            # a0 = clz(a0)
     li t3, 31
     sub t0, t3, a0          # msb = t0 = 31 - clz(a0)
 
     lw a0, 0(sp)            # restore a0 from stack
-    addi sp, sp, 4
+    lw ra, 4(sp)            # restore ra from stack
+    addi sp, sp, 8
 
     li t1, 0                # exp = t1 = 0
     li t2, 0                # of = t2 = 0
@@ -71,7 +75,7 @@ fine_exa_exp:
     bge t1, t3, cmb_num         # if (exp >= 15)
     slli t0, t2, 1              # t0 = of << 1
     addi t0, t0, 16             # t0 = (of << 1) + 16 = of_e+1
-    bge a0, t0, cmb_num         # if (a0 >= of_e) cmb_num
+    blt a0, t0, cmb_num         # if (a0 >= of_e) cmb_num
     mv t2, t0                   # of = of_e
     addi t1, t1, 1              # exp++
 
@@ -98,7 +102,7 @@ clz_loop:
 
 skip_update:
     srli t1, t1, 1           # c = c / 2
-    bnez t1, clz_loop        # while (c) loop
+    bgtz t1, clz_loop        # while (c) loop
     sub  a0, t0, a0          # return n - x
     ret
 
